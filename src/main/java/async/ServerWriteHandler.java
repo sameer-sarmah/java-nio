e66fa8b7150b248882a906b9597e50f1fd0bc31b
Sameer Sarmah <sameer.paran.sarmah@sap.com>
2018-05-22 16:53:54 +0530

    refactoring

--- src/main/java/async/ServerWriteHandler.java
+++ src/main/java/async/ServerWriteHandler.java
@@ -1,5 +1,7 @@
 package async;
 
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 
 public class ServerWriteHandler implements CompletionHandler<Integer, ServerReqResObject> {
@@ -10,6 +12,10 @@ public class ServerWriteHandler implements CompletionHandler<Integer, ServerReqR
 		  String productJson=reqResObject.getProductJsonResponse();
 		  System.out.println("response for product id "+productID +" is: ");
 		  System.out.println(productJson);
+		  AsynchronousSocketChannel client = reqResObject.getChannelClient();
+		  ByteBuffer bufferForProductID = ByteBuffer.allocateDirect(100);
+		  reqResObject.setBufferForProductID(bufferForProductID);
+		  client.read(bufferForProductID, reqResObject, new ServerReadHandler());
 		 
 	}
 
